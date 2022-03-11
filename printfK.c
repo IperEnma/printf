465e8138c3ac42e5a50cb6d7d24461d14dea6d87
KevinFRX <xerokf13@gmail.com>
2022-03-11 12:18:05 -0800

    XD

--- printfK.c
+++ printfK.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
 * _printf - printf
@@ -9,5 +10,22 @@
 
 int _printf(const char *format, ...)
 {
-	
+	va_list p;
+	unsigned int i;
+
+	pr pf_s[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_%},
+	};
+
+	va_start(p, format);
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != "%")
+			write(1, format[i], 1);
+		else
+			
+	}
 }
