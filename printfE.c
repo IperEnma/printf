cbb0012a66866e7f8e9db7d5f7de7ee1e1b7d501
IperEnma <enmanuelhernandez1843.com>
2022-03-11 12:17:41 -0800

    printf

--- printfE.c
+++ printfE.c
@@ -1,13 +1,50 @@
+#include <stderg.h>
 /**
  *
  *
  *
  */
-int _printf(const char * const input, ...)
+void print_char(va_list c)
 {
+	char aux;
 
-	pr ms[] =
+	aux = va_arg(c, format);
+
+	putchar(aux);
+}
+/**
+ *
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+	va_list p;
+	char *tmp;
+	int i = 0, j = 0, k = 0;
+
+
+	pr pf_s[] =
        	{
-		{char
+		{"s", print_string}
+		{"c", print_char}
+		{"%", print_%}
 	};
+
+	va_start(p, input);
+
+	for (i = 0; input[i]; i++)
+
+	{	if (input[i] == "%")
+		{	
+			i++;
+
+			for (j = 0; pf_s[j]; j = 0)
+			{
+				if (input[i] = pf_s[j].c)
+					pf_s[j].f(p);
+			}
+		}
+	}
+
 }
