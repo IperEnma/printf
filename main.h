16c67d36e5ed6e061e9c6c6f256f7cea6aad53cc
IperEnma <enmanuelhernandez1843.com>
2022-03-11 16:15:21 -0800

    recovery

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef PRINTF
+#define PRINTF
+typedef struct prf
+{
+	char c;
+	void (*f)(va_list p);
+
+} pr;
+int _printf(const char *format, ...);
+int _putchar(char c);
+#endif
---
+++ printfK.c
@@ -0,0 +1,83 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_mod(va_list m)
+{
+	_putchar(37);
+}
+/**
+ *
+ *
+ *
+ */
+void print_string(va_list s)
+{
+	int i = 0;
+	char *aux = va_arg(s, char *);
+
+	for (i = 0; aux[i]; i++)
+	{
+		_putchar(aux[i]);
+	}
+}
+/**
+ *
+ *
+ *
+ */
+void print_char(va_list c)
+{
+	char aux;
+
+	/*printf("hola\n");*/
+	aux = va_arg(c, int);
+
+	_putchar(aux);
+}
+/**
+ *
+ *
+ *
+ */
+int _printf(const char *format, ...)
+{
+	va_list p;
+	char *tmp;
+	int i = 0, j = 0, k = 0;
+
+
+	pr pf_s[] =
+       	{
+		{'s', print_string},
+		{'c', print_char},
+		{'%', print_mod},
+		{'\0', NULL}
+	};
+
+	va_start(p, format);
+
+	for (i = 0; format[i]; i++)
+
+	{	if (format[i] == '%')
+		{	
+			/*printf("hola estoy en el if\n");*/
+			i++;
+
+			for (j = 0; pf_s[j].c; j++)
+			{
+
+				/*printf("hola estoy en el segundo for\n");*/
+				if (format[i] == pf_s[j].c)
+					pf_s[j].f(p);
+			}
+		}
+		else
+			_putchar(format[i]);
+	}
+
+}
