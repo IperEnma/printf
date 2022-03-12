f6de9f4c4dcb7ddff00945594cb6d8d65996ac14
KevinFRX <xerokf13@gmail.com>
2022-03-12 09:03:10 -0800

    POR FIIIN

--- printfK.c
+++ printfK.c
@@ -9,37 +9,27 @@
  */
 int print_dec(va_list i)
 {
-	int a, b, c, d = 1, n;
+	int b, c = 1, d = 1, n;
 
 	n = va_arg(i, int);
+	b = n;
 
-	if (n == -2147483648)
+	if (n < 0)
 	{
-		_putchar('-');
-		_putchar('2');
-		_putchar('1');
-		_putchar('4');
-		_putchar('7');
-		_putchar('4');
-		_putchar('8');
-		_putchar('3');
-		_putchar('6');
-		_putchar('4');
-		_putchar('8');
+		_putchar(45);
+		while (b <= -10)
+		{
+			b /= 10;
+			c *= 10;
+			d++;
+		}
+		for (; c >= 1; c /= 10)
+		{
+			_putchar((((n / c) % 10) * -1) + 48);
+		}
 	}
 	else
 	{
-		if (n < 0)
-		{
-			a = n * -1;
-			_putchar(45);
-		}
-		else
-		{
-			a = n;
-		}
-		b = a;
-		c = 1;
 		while (b >= 10)
 		{
 			b /= 10;
@@ -48,8 +38,7 @@ int print_dec(va_list i)
 		}
 		for (; c >= 1; c /= 10)
 		{
-			_putchar(((a / c) % 10) + 48);
-		
+			_putchar(((n / c) % 10) + 48);
 		}
 	}
 	return (d);
