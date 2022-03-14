67c557ea1c403442b925e79904fbc513beeedb41
IperEnma <enmanuelhernandez1843.com>
2022-03-14 13:33:31 -0700

    final de los mandatorios

--- print_int.c
+++ print_int.c
@@ -2,39 +2,48 @@
 #include <string.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * print_int - print integer
- * @i: valist
- * Return: aux
+ * print_int - prints a integer
+ * @i: the integer
+ *
+ * Return: the amount of digits of the number
  */
+
 int print_int(va_list i)
 {
-	int c = va_arg(i, int), j = 1, w = 0;
-	char *aux;
+	int b, c = 1, ret = 1, n;
 
-	w = c;
+	n = va_arg(i, int);
+	b = n;
 
-	while (w >= 10)
+	if (n < 0)
 	{
-		w = w / 10;
-		j++;
+		ret = 2;
+		_putchar(45);
+		while (b <= -10)
+		{
+			b /= 10;
+			c *= 10;
+			ret++;
+		}
+		for (; c >= 1; c /= 10)
+		{
+			_putchar((((n / c) % 10) * -1) + 48);
+		}
 	}
-
-	aux = malloc(j);
-
-	if (c < 0)
+	else
 	{
-		_putchar('-');
-		c = (c * -1);
+		while (b >= 10)
+		{
+			b /= 10;
+			c *= 10;
+			ret++;
+		}
+		for (; c >= 1; c /= 10)
+		{
+			_putchar(((n / c) % 10) + 48);
+		}
 	}
-
-	for (j = 0; c != 0; j++)
-	{
-		aux[j] = c % 10;
-		c = c / 10;
-	}
-	for (j = (strlen(aux) - 1); j >= 0; j--)
-		_putchar(aux[j] + '0');
-	free(aux);
-	return (strlen(aux) - 1);
+	return (ret);
 }
