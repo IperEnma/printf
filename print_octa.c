07cf874e15233428f2d41160b2c3de88d2ba462a
IperEnma <enmanuelhernandez1843.com>
2022-03-14 17:29:23 -0700

    octa

--- print_octa.c
+++ print_octa.c
@@ -1,32 +1,32 @@
 #include "main.h"
 /**
- * aux_mod - helper to print
+ * aux_octa - helper to print
  * @b: number to convert
  * Return: void
  */
 void aux_octa(unsigned int b)
 {
-        if (b / 8)
-        {
-                aux_octa(b / 8);
-                _putchar(b % 8 + 48);
-        }
-        else
-                _putchar(b % 8 + 48);
+	if (b / 8)
+	{
+		aux_octa(b / 8);
+		_putchar(b % 8 + 48);
+	}
+	else
+		_putchar(b % 8 + 48);
 }
 /**
- * print_bin - convert number to binary
- * @b: valist
+ * print_octa - convert number to octa
+ * @o: valist
  * Return: i
  */
 int print_octa(va_list o)
 {
-        int i = 1;
-        unsigned int oct = va_arg(o, unsigned int);
+	int i = 1;
+	unsigned int oct = va_arg(o, unsigned int);
 
-        aux_octa(oct);
+	aux_octa(oct);
 
-        for (i = 1; oct / 8; i++)
-                oct = oct / 8;
-        return (i);
+	for (i = 1; oct / 8; i++)
+		oct = oct / 8;
+	return (i);
 }
