324d1978e6f60c2fe1c1452125bffd77cab389ab
IperEnma <enmanuelhernandez1843.com>
2022-03-16 08:40:48 -0700

    update

--- print_ascii.c
+++ print_ascii.c
@@ -5,15 +5,6 @@
 #include <stdio.h>
 /**
  *
- *
- *
- *
-int aux_ascii(char c)
-{
-	putchar(c);
-
-}
- *
  * print_ascii - convert char no printable
  * @ascii: parameter
  * Return: ret
@@ -25,23 +16,23 @@ int print_ascii(va_list s)
 	
 
 	if (as == NULL)
-		as = "(null)";
+		as = "(nil)";
 	for (i = 0; as[i]; i++)
 	{
 		if  ((as[i] > 0 && as[i] < 32) || as[i] >= 127)
 		{	
-			_putchar(92);
-			_putchar('x');	
+			ret += _putchar(92);
+			ret += _putchar('x');	
 			if (as[i] <  16)
 			{
-				_putchar('0');
-				_printf("%X", as[i]);
+				ret +=_putchar('0');
+				ret +=_printf("%X", as[i]);
 			}
 			else
-				_printf("%X", as[i]);
+				ret +=_printf("%X", as[i]);
 		}
 		else
-			_putchar(as[i]);
+			ret +=_putchar(as[i]);
 	}
-	return (ret++);
+	return (ret);
 }
